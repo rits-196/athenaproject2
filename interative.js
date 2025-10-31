00d61069f97b28a714ec332a872fdd3c08260acb
rits-196 <rits.shah103@gmail.com>
2025-10-31 11:22:05 +0000

    trying to make an interactive fashion link

---
+++ interative.js
@@ -0,0 +1,27 @@
+const draggables = document.querySelectorAll('.draggable');
+
+draggables.forEach(item => {
+  item.addEventListener('mousedown', dragStart);
+});
+
+function dragStart(e) {
+  const element = e.target;
+  let shiftX = e.clientX - element.getBoundingClientRect().left;
+  let shiftY = e.clientY - element.getBoundingClientRect().top;
+
+  function moveAt(pageX, pageY) {
+    element.style.left = pageX - shiftX + 'px';
+    element.style.top = pageY - shiftY + 'px';
+  }
+
+  function onMouseMove(e) {
+    moveAt(e.pageX, e.pageY);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  element.onmouseup = function () {
+    document.removeEventListener('mousemove', onMouseMove);
+    element.onmouseup = null;
+  };
+}
